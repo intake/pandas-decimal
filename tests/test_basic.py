934ff5f72855adaf280dcd5e5f94c6f0c3685063
Ryan Keith <rkeith@anaconda.com>
2022-11-09 14:02:01 -0800

    Adding another test.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,2 +1,3 @@
 def test_if_basic_import_works():
+    # This slightly tests for syntax errors as well
     import pandas_decimal
--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,2 +1,6 @@
 import pytest
 
+from pandas_decimal import DecimaldDtype
+
+def test_adding_works():
+    pass
