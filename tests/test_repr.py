bd133429805520f948be5836bb45e0dfaa12c788
Martin Durant <martin.durant@alumni.utoronto.ca>
2022-11-10 13:34:38 -0500

    more tests of that

--- tests/test_repr.py
+++ tests/test_repr.py
@@ -4,12 +4,20 @@ import pandas_decimal
 
 
 @pytest.mark.parametrize(
+    "func", [str, repr]
+)
+@pytest.mark.parametrize(
     "data", [
-        [[1, 0], "1\n"]
+        [[1, 0], "1\n"],
+        [[1, 1], "1.0\n"],
+        [[[1, 1], 1], "1.0\n"],
+        [[[1, 1], 3], "1.000\n"],
+        [[[0.0001], 3], "0.000\n"],
+        [[[0.1, 1], 3], "0.100\n"]
     ]
 )
-def test_series_repr(data):
+def test_series_repr(data, func):
     (values, places), expected = data
     s = pd.Series(values, dtype=pandas_decimal.DecimaldDtype(places))
-    out = repr(s)
+    out = func(s)
     assert expected in out
