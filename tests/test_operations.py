dab27b9094d1161535514e88b698d9efb638598f
Ryan Keith <rkeith@anaconda.com>
2022-11-10 13:07:44 -0800

    Adding more adding tests.

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -14,7 +14,7 @@ def test_adding_of_same_degree_works(degree):
     # Test degree didn't change
     assert str(x.dtype) == dtype_str
     # Test values are correct
-    assert all([np.isclose(x, y) for x, y in zip(expected, z)])
+    assert all([i == j for i, j in zip(expected, z)])
 
 @pytest.mark.parametrize("degree", [0,1,2,3])
 def test_adding_of_same_degree_works2(degree):
@@ -28,3 +28,35 @@ def test_adding_of_same_degree_works2(degree):
     assert str(x.dtype) == dtype_str
     # Test values are correct
     assert np.all(expected == z)
+"""
+@pytest.mark.parametrize("delta_degree", [1,2,3])
+def test_adding_of_different_degree_works(delta_degree):
+    degree = 0
+    other_degree = degree + delta_degree
+    dtype_str = f'decimal[{degree}]'
+    original = [0,1,2,3]
+    expected = [x+y for x,y in zip(original, original)]
+    x = pd.Series(original, dtype=dtype_str)
+    y = pd.Series(original, dtype=dtype_str)
+    z = x + y
+    # Test degree didn't change
+    assert str(x.dtype) == dtype_str
+    # Test values are correct
+    assert all([np.isclose(x, y) for x, y in zip(expected, z)])
+
+@pytest.mark.parametrize("delta_degree", [1,2,3])
+def test_adding_of_different_degree_works2(delta_degree):
+    degree = 0
+    other_degree = degree + delta_degree
+    dtype_str = f'decimal[{degree}]'
+    original = [0,1,2,3]
+    expected = [x+y for x,y in zip(original, original)]
+    x = pd.Series(original, dtype=dtype_str)
+    y = pd.Series(original, dtype=dtype_str)
+    z = x + y
+    # Test degree didn't change
+    assert str(x.dtype) == dtype_str
+    # Test values are correct
+    assert np.all(expected == z)
+
+"""
