1c12a94a7fdab85a0e4a7691440887d5a09c1c02
Ryan Keith <rkeith@anaconda.com>
2022-11-10 12:44:24 -0800

    Seems addition on single values is broken but not when compared using a sequence.

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,6 +1,30 @@
 import pytest
 
-from pandas_decimal import DecimaldDtype
+import numpy as np
+import pandas as pd
 
-def test_adding_works():
-    pass
+@pytest.mark.parametrize("degree", [0,1,2,3])
+def test_adding_of_same_degree_works(degree):
+    dtype_str = f'decimal[{degree}]'
+    original = [0,1,2,3]
+    expected = [x+y for x,y in zip(original, original)]
+    x = pd.Series(original, dtype=dtype_str)
+    y = pd.Series(original, dtype=dtype_str)
+    z = x + y
+    # Test degree didn't change
+    assert str(x.dtype) == dtype_str
+    # Test values are correct
+    assert all([np.isclose(x, y) for x, y in zip(expected, z)])
+
+@pytest.mark.parametrize("degree", [0,1,2,3])
+def test_adding_of_same_degree_works2(degree):
+    dtype_str = f'decimal[{degree}]'
+    original = [0,1,2,3]
+    expected = [x+y for x,y in zip(original, original)]
+    x = pd.Series(original, dtype=dtype_str)
+    y = pd.Series(original, dtype=dtype_str)
+    z = x + y
+    # Test degree didn't change
+    assert str(x.dtype) == dtype_str
+    # Test values are correct
+    assert np.all(expected == z)
